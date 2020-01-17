c2f23e1009ae33acd97ff618f9963f403ef54e5e
Franicisco Leyva <surefirefjl@gmail.com>
2020-01-17 09:32:58 -0600

    longest palindrome complete

--- longestPalindrome/prompt.js
+++ longestPalindrome/prompt.js
@@ -5,5 +5,30 @@
  * palindromes in the above string include "dad", "ete", " dad " (including
  * whitespace on each side of dad).
  */
+var longestPalindrome = (string) => {
+  if(!string || string.length <= 1) {
+    return string
+  }
+  let longest = string.substring(0, 1)
+  for(let i = 0; i < string.length; i++) {
+    let temp = expand(string, i, i)
+    if(temp.length > longest.length) {
+      longest = temp
+    }
+    temp = expand(string, i, i + 1)
+    if(temp.length > longest.length) {
+      longest = temp
+    }
+  }
+  return longest
+}
 
-var longestPalindrome = function(string) {};
+const expand = (s, begin, end) => {
+  while(begin >= 0 && end <= s.length - 1 && s[begin] === s[end]) {
+    begin--
+    end++
+  }
+  return s.substring(begin + 1, end)
+}
+
+console.log(longestPalindrome('My dad is a racecar athlete'))
